d37c8378f014cd549314bfbba67a64bb0438d5ab
Fedyna-Nataliia <0960011248@ukr.net>
2020-09-03 22:52:14 +0300

    виконує дз2 з1

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,15 @@
 
-
 const getItemsString = function(array) {
     'use strict';
     // Write code under this line
-    for (let i = 0; i <= getItemsString.length -1; i =+ 1) {
-      console.log(getItemsString[i]);
-    };
-  };
+    let result = ''
+    for (let i = 0; i <= array.length-1; i += 1) {
+     result += `${i + 1} - ${array[i]}\n`
+    }
+    return result
+
+   };
+  
   
   console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
   /*
