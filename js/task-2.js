5dafa3fbf6f75ccee0ca4614b3195edf19ee9f76
Fedyna-Nataliia <0960011248@ukr.net>
2020-09-03 21:21:27 +0300

    робить розмітку і підключає файли

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+
+
+const getItemsString = function(array) {
+    'use strict';
+    // Write code under this line
+    for (let i = 0; i <= getItemsString.length -1; i =+ 1) {
+      console.log(getItemsString[i]);
+    };
+  };
+  
+  console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+  /*
+  '1 - Mango
+  2 - Poly
+  3 - Ajax
+  4 - Lux
+  5 - Jay
+  6 - Kong
+  '
+  */
+  
+  console.log(getItemsString([5, 10, 15]));
+  /*
+  '1 - 5
+  2 - 10
+  3 - 15
+  '
+  */
