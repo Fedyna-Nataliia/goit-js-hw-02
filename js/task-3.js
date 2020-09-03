862a789359ea8fcf38dd064eedaad746ee59515b
Fedyna-Nataliia <0960011248@ukr.net>
2020-09-04 00:17:37 +0300

    записує умову задачі

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+const calculateEngravingPrice = (message = "", pricePerWord = 0) => pricePerWord;
+let message = ''
+let pricePerWord = ''
+
+console.log(pricePerWord = (message.split('').length) * pricePerWord);
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
