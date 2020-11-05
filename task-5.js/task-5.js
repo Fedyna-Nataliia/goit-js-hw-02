10b98d2cf32e61ec9345922b0aa158f74ad2db6c
Fedyna-Nataliia <0960011248@ukr.net>
2020-11-05 16:43:18 +0300

    закінчує з 5 модуль5

--- task-5.js/task-5.js
+++ task-5.js/task-5.js
@@ -1,3 +1,121 @@
+   
+    /*
+    Задача 5-5
+класс
+Напиши класс Car с указанными свойствами и методами.
+
+class Car {
+ 
+   * Добавь `статический` метод 
+   * `getSpecs(car)`, который принимает 
+   * объект-машину как параметр 
+   * и возвращает шаблонную строку 
+   * со свойствами и значениями объекта.
+   * Свойства: 
+   *   maxSpeed, 
+   *   speed, 
+   *   isOn, 
+   *   distance,
+   *   price
+   * Пример строки, полученной этим методом:
+   * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+ 
+
+ 
+   * Конструктор получает объект настроек.
+   *
+   * Добавь свойства будущеего экземпляра класса:
+   *  speed - текущая скорость, 
+   *          начальное значение `0`
+   *  price - цена автомобиля
+   *  maxSpeed - максимальная скорость
+   *  isOn - заведен ли автомобиль. 
+   *         Значения `true` или `false`,  
+   *         начальное значение false
+   *  distance - пробег в километрах, 
+   *             начальное значение `0`
+  
+   constructor() {}
+
+  
+   * Добавь геттер и сеттер 
+   * для свойства `price`, который будет 
+   * работать с свойством цены автомобиля.
+   * 
+   * ВАЖНО: для записи методов get и set
+   * значение параметра записывают с 
+   * подчеркиванием. См. ниже пример 1.
+  
+   * Метод, который заводит автомобиль
+   * Записывает в свойство `isOn` значение `true`
+  
+  turnOn() {}
+
+
+   * Метод, чтобы заглушить автомобиль
+   * Этот метод должен записывать 
+   * в свойство isOn значение false,
+   * и сбрасывать текущую скорость до 0
+  
+ turnOff() {}
+
+  /*
+   * Этот метод должен добавлять 
+   * к свойству `speed` полученное
+   * значение, при условии, 
+   * что результирующая скорость
+   * не больше чем значение свойства `maxSpeed`
+ 
+ accelerate(value) {}
+
+
+   * Этот метод должен отнимать 
+   * от свойства `speed` 
+   * полученное значение, при условии, 
+   * что результирующая скорость не меньше 0
+  
+
+ decelerate(value) {}
+
+  
+   * Этот метод должен добавлять к свойству 
+   * `distance` пробег в километрах, 
+   * т.е. hours * speed,
+   * но только в том случае, 
+   * если машина заведена!
+   
+  drive(hours) {}
+}
+
+#### Пример 1.
+
+```js
+class Guest {
+  Собственные свойства класса размещаем в конструкторе
+  constructor(name, roomNumber) {
+     параметр name не должен 
+     *   совпадать с названием свойства
+     *   но должен быть похож.
+     *   Поэтому используют прием смены
+     *   названия добавляя символ '_'.
+     *   Хотя это может любой другой символ
+   
+   this._name = name;
+  }
+
+   Используем геттеры и сеттеры для описания интерфейса доступа к свойствам
+  get name() {
+  return this._name;
+  }
+
+ set name(value) {
+   this._name = value;
+ }
+}
+   */
+    
+    //    Варіант 1
+   
    class Car {
     // Write code under this line
     static getSpecs(car) {
@@ -5,7 +123,7 @@
    }
      constructor(car) {
       this.speed = 0;
-      this._price = price;
+      this._price = car.price;
       this.maxSpeed = car.maxSpeed;
       this.isOn = false;
       this.distance = 0;
@@ -27,12 +145,16 @@
      accelerate(value) {
          if((value + this.speed) < this.maxSpeed) {
             this.speed += value;
-         }
+         } else {
+            this.speed = this.maxSpeed;
+          }
      }
      decelerate(value) { 
          if((this.speed - value) > 0) {
         this.speed -= value;
-     }
+     } else {
+      this.speed = 0;
+    }
     }
      drive(hours) {
          if(this.isOn === true) {
@@ -60,3 +182,51 @@
    mustang.price = 4000;
     console.log(mustang.price); // 4000
    
+/*    Варіант 2
+Також можна використати дестуруктуризацію:
+class Car {
+ // Write code under this line
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
+  }
+  constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value;
+    } else {
+      this.speed = this.maxSpeed;
+    }
+  }
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed -= value;
+    } else {
+      this.speed = 0;
+    }
+  }
+  drive(hours) {
+    if (this.isOn === true) {
+      this.distance += hours * this.speed;
+    }
+  }
+}
+*/
